b34870ef40392a78d0d28eb6d627fd2831b8a59d
tattii <yuta.tatti@gmail.com>
2017-06-12 05:30:17 +0900

    parse warning xml

--- warning.py
+++ warning.py
@@ -12,37 +12,85 @@ def parse(url):
     if xml['Report']['Control']['Status'] != u'通常':
         return
 
-    data = {}
-
-    print xml['Report']['Head']['Title']
+    # head
     timestr = xml['Report']['Head']['ReportDateTime']
     report_time = dt.strptime(timestr[:-6], "%Y-%m-%dT%H:%M:%S")
-    print report_time
-    print xml['Report']['Head']['Headline']['Text']
+    comment = xml['Report']['Head']['Headline']['Text']
 
-    pref_warning = xml['Report']['Body']['Warning'][0]
-    for kind in pref_warning['Item']['Kind']:
-        print kind['Name']
+    print xml['Report']['Head']['Title'], report_time
 
-    print pref_warning['Item']['Area']['Code']
+    # 気象警報・注意報（府県予報区等）
+    prefdata = {}
+    pref_warnings = xml['Report']['Body']['Warning'][0]
+    items = pref_warnings['Item']
+    if not isinstance(items, list): items = [items]
+    for pref in items:
+        warnings = warnings_tolist(pref)
+        code = pref['Area']['Code']
 
+        prefdata[code] = {
+            'name': pref['Area']['Name'],
+            'status': warning_status(warnings),
+            'warnings': pref_warnings,
+            'report_time': report_time,
+            'comment': comment
+        }
 
+    # 気象警報・注意報（市町村等）
     city_warnings = xml['Report']['Body']['Warning'][3]
+    citydata = parse_city_warnings(city_warnings)
+
+    # 量的予想時系列（市町村等）
+    # 複雑....
+
+    return prefdata, citydata
+
+
+def parse_city_warnings(city_warnings):
+    citydata = {}
     for city in city_warnings['Item']:
-        print city['Area']['Name']
+        code = city['Area']['Code']
+        warnings = warnings_tolist(city)
+        
+        citydata[code] = {
+            'name': city['Area']['Name'],
+            'status': warning_status(warnings),
+            'warnings': warnings
+        }
 
-        if isinstance(city['Kind'], list):
-            for kind in city['Kind']:
-                print kind['Name']
+    return citydata
 
+
+def warnings_tolist(item):
+    warnings = []
+
+    if isinstance(item['Kind'], list):
+        for kind in item['Kind']:
+            warnings.append(kind['Name'])
+
+    else:
+        if item['Kind']['Status'] != u'発表警報・注意報はなし':
+            warnings.append(item['Kind']['Name'])
+
+    return warnings
+
+
+def warning_status(warnings):
+    # emergency | warning | advisory | none
+    status = 'none'
+    if len(warnings):
+        if len([w for w in warnings if w[:-4] == u'特別警報']):
+            status = 'emergency'
+        elif len([w for w in warnings if w[:-2] == u'警報']):
+            status = 'warning'
         else:
-            if city['Kind']['Status'] != u'発表警報・注意報はなし':
-                print city['Kind']['Name']
+            status = 'advisory'
 
-    return data
+    return status
 
 
 if __name__ == '__main__':
     import sys
-    parse(sys.argv[1])
+    prefdarta, citydata = parse(sys.argv[1])
+
 
