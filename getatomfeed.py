0d60cef63b401c0a8b9fc842aaa69b911937e094
tattii <yuta.tatti@gmail.com>
2017-06-16 02:20:50 +0900

    getatomfeed init, fetch

--- getatomfeed.py
+++ getatomfeed.py
@@ -1,19 +1,49 @@
 # -*- coding: utf-8 -*-
+from datetime import datetime as dt
 import feedparser
 
+import warning
+
 url = 'http://www.data.jma.go.jp/developer/xml/feed/extra.xml'
+url_long = 'http://www.data.jma.go.jp/developer/xml/feed/extra_l.xml'
 
 def fetch():
     atom = feedparser.parse(url)
-    print atom.updated
+    last_update_utc = warning.check()
+    xmllist = []
+    print last_update_utc
 
     for entry in atom.entries:
         if entry.title == u'気象警報・注意報（Ｈ２７）':
-            print entry.title, entry.content[0]['value']
-            print entry.links[0]['href']
+            updated_utc = dt.strptime(entry.updated[:-1], "%Y-%m-%dT%H:%M:%S")
+            print updated_utc
 
+            if updated_utc > last_update_utc:
+                print entry.title, entry.content[0]['value'], entry.updated
+                xmllist.append(entry.links[0]['href'])
+
+    if len(xmllist):
+        warning.processall(reversed(xmllist))
+
+
+def init():
+    atom = feedparser.parse(url_long)
+    xmlpref = {}
+
+    for entry in atom.entries:
+        if entry.title == u'気象警報・注意報（Ｈ２７）':
+            pref = entry.content[0]['value'][1:4]
+
+            if not pref in xmlpref:
+                xmlpref[pref] = entry.links[0]['href']
+
+    warning.processall(xmlpref.values(), init=True)
 
 
 if __name__ == '__main__':
-    fetch()
+    import sys
+    if len(sys.argv) > 1:
+        init()
+    else:
+        fetch()
 
--- warning.py
+++ warning.py
@@ -1,14 +1,28 @@
 # -*- coding: utf-8 -*-
 import json
 from datetime import datetime as dt
+import datetime
+import codecs
 
 import warningxml
 
 import boto3
 s3_client = boto3.client('s3')
 
-def check():
-    return
+
+def processall(urllist, init=False):
+    if init:
+        prefjson = {}
+        cityjson = {}
+    else:
+        prefjson = download('pref')
+        cityjson = download('city')
+
+    for url in urllist:
+        prefjson, cityjson = process(url, prefjson, cityjson)
+
+    upload('pref', prefjson)
+    upload('city', cityjson)
 
 
 def process(url, prefjson, cityjson):
@@ -17,12 +31,19 @@ def process(url, prefjson, cityjson):
     for prefcode in prefdata.keys():
         prefjson[prefcode] = prefdata[prefcode]
     prefjson['lastupdate'] = report_time
-    print json.dumps(prefjson, ensure_ascii=False)
 
     for citycode in citydata.keys():
         cityjson[citycode] = citydata[citycode]
     cityjson['lastupdate'] = report_time
-    print json.dumps(cityjson, ensure_ascii=False)
+
+    return prefjson, cityjson
+
+
+def check():
+    prefdata = download('pref')
+    last_update = dt.strptime(prefdata['lastupdate'], "%Y%m%d%H%M%S")
+    last_update_utc = last_update - datetime.timedelta(hours=9)
+    return last_update_utc
 
 
 def download(type):
@@ -31,17 +52,19 @@ def download(type):
     s3_client.download_file('vector-tile', key, file)
 
     with open(file) as f:
-        return json.load(f)
+        return json.loads(f.read(), 'utf-8')
 
 def upload(type, jsondata):
     key = 'warning/' + type + '.json'
     jsonfile = '/tmp/' + type + '.json'
 
-    with open(jsonfile, 'w') as f:
+    with codecs.open(jsonfile, 'w', 'utf-8') as f:
         f.write(json.dumps(jsondata, ensure_ascii=False))
 
-        s3_client.upload_file(jsonfile, 'vector-tile', key,
-            ExtraArgs={'ContentType': "application/json"})
+    s3_client.upload_file(jsonfile, 'vector-tile', key,
+        ExtraArgs={
+            'ContentType': 'application/json; charset=utf-8',
+            'ACL': 'public-read'})
 
 
 if __name__ == '__main__':
